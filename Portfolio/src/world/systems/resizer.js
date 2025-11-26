da67940e1a1206ee276d1b21e98729b1a17e5fd0
REUBEN BURTON <>
2025-11-26 17:19:18 +0000

    Fixed problems with last push

--- Portfolio/src/world/systems/resizer.js
+++ Portfolio/src/world/systems/resizer.js
@@ -1,12 +1,12 @@
 const setSize = (camera, renderer, maxPixelCount = 3840*2160) => {
     const canvas = renderer.domElement;
-    const pixelRatio = window.pixelRatio;
+    const pixelRatio = window.devicePixelRatio;
     let width = Math.floor(canvas.clientWidth * pixelRatio);
     let height = Math.floor(canvas.clientHeight * pixelRatio);
     const pixelCount = width * height;
     const renderScale = pixelCount > maxPixelCount ? Math.sqrt(maxPixelCount / pixelCount): 1;
     width = Math.floor(width * renderScale);
-    height = Math.floor(hieght * renderScale);
+    height = Math.floor(height * renderScale);
 
 
     if (width !== canvas.width || height !== canvas.height){
